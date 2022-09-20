e4ea5e1343037472cde486ca4ff08117de174bee
kminoda <koji.minoda@tier4.jp>
2022-09-20 16:44:45 +0900

    resolve conflicts
    Signed-off-by: kminoda <koji.minoda@tier4.jp>

--- localization/ndt_scan_matcher/include/ndt_scan_matcher/ndt_scan_matcher_core.hpp
+++ localization/ndt_scan_matcher/include/ndt_scan_matcher/ndt_scan_matcher_core.hpp
@@ -113,15 +113,7 @@ std::shared_ptr<NormalDistributionsTransformBase<PointSource, PointTarget>> copy
   if (ndt_mode == NDTImplementType::OMP_MULTI_VOXEL) {
     using T = NormalDistributionsTransformOMPMultiVoxel<PointSource, PointTarget>;
     std::shared_ptr<T> input_ndt_tmp_ptr = std::dynamic_pointer_cast<T>(input_ndt_ptr);
-<<<<<<< HEAD
     return std::make_shared<T>(*input_ndt_tmp_ptr);
-=======
-    std::shared_ptr<T> output_ndt_tmp_ptr = std::dynamic_pointer_cast<T>(output_ndt_ptr);
-    // output_ndt_tmp_ptr->copyFrom(*input_ndt_tmp_ptr);
-
-    T copied_ndt_tmp = *input_ndt_tmp_ptr;
-    output_ndt_tmp_ptr = std::make_shared<T>(copied_ndt_tmp);
->>>>>>> feature/differential_dynamic_map_loading
   } else {
     const std::string s = fmt::format("Unknown NDT type {}", static_cast<int>(ndt_mode));
     throw std::runtime_error(s);
